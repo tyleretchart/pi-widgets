046684f72cf81a4cc7e4cdd1343a79296136d44c
Tyler Etchart <tyler.etchart@gmail.com>
2018-10-12 17:49:45 -0600

    light button now mapped to send to defined host

--- berry_base/src/communication.rs
+++ berry_base/src/communication.rs
@@ -2,7 +2,7 @@ use std::io::prelude::*;
 use std::io::{self, Read};
 use std::net::TcpStream;
 
-pub fn connect(address: &String, data: String) -> String {
+pub fn connect_with_gui(address: &String, data: String) -> String {
     let mut stream = TcpStream::connect(address).expect("Couldn't connect to the server...");
     stream
         .set_nonblocking(true)
@@ -24,4 +24,28 @@ pub fn connect(address: &String, data: String) -> String {
     }
     println!("bytes: {:?}", buf);
     String::from_utf8(buf).expect("Found invalid UTF-8")
+}
+
+pub fn connect_with_light(address: &String, data: String) {
+    let mut stream = TcpStream::connect(address).expect("Couldn't connect to the server...");
+    stream
+        .set_nonblocking(true)
+        .expect("set_nonblocking call failed");
+
+    let _ = stream.write(data.as_bytes()).unwrap();
+
+    // let mut buf = vec![];
+    // loop {
+    //     match stream.read_to_end(&mut buf) {
+    //         Ok(_) => break,
+    //         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+    //             // wait until network socket is ready, typically implemented
+    //             // via platform-specific APIs such as epoll or IOCP
+    //             // wait_for_fd();
+    //         }
+    //         Err(e) => panic!("encountered IO error: {}", e),
+    //     };
+    // }
+    // println!("bytes: {:?}", buf);
+    // String::from_utf8(buf).expect("Found invalid UTF-8")
 }
--- berry_base/src/main.rs
+++ berry_base/src/main.rs
@@ -8,61 +8,24 @@
 
 extern crate serde;
 extern crate serde_json;
+extern crate tokio;
+extern crate tokio_codec;
 
 #[macro_use]
 extern crate serde_derive;
-
-use std::env;
-
-mod button;
-mod communication;
-mod light;
-
-// fn main() {
-//     // collect address
-//     let args: Vec<String> = env::args().collect();
-//     if args.len() != 2 {
-//         println!("Usage: ./run <ip:port>");
-//     }
-//     let address: String = args[1].parse().unwrap();
-
-//     // default data
-//     let mut data = String::from("{\"pin\": 18, \"duration_ms\": 800, \"period_ms\": 200}");
-
-//     // poll buttons
-//     loop {
-//         let code_change_pressed = button::poll(26);
-//         println!("CODE CHANGE BUTTON: {}", code_change_pressed);
-//         if code_change_pressed {
-//             let data_tmp = communication::connect(&address, data.to_string());
-//             data = String::from(data_tmp);
-//         }
-//         let light_activate_pressed = button::poll(27);
-//         println!("LIGHT ACTIVATE BUTTON: {}", light_activate_pressed);
-//         if light_activate_pressed {
-//             let light_args: light::LightArguments = serde_json::from_str(&data).unwrap();
-//             light::blink_led(light_args);
-//         }
-//         println!("");
-//     }
-// }
-
-// ========================================================================================
-
-// #![deny(warnings)]
-
-extern crate tokio;
-extern crate tokio_codec;
 
 use tokio::codec::Decoder;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 use tokio_codec::BytesCodec;
 
-// use std::env;
+use std::env;
 use std::net::SocketAddr;
-
 use std::thread;
+
+mod button;
+mod communication;
+mod light;
 
 fn main() {
     // collect addresses
@@ -76,7 +39,7 @@ fn main() {
 
     // default data for button
     let mut data = String::from(
-        "{\"host\": \"boysenberry\", \"pin\": 18, \"duration_ms\": 800, \"period_ms\": 200}",
+        "{\"host\": \"boysenberry:5002\", \"pin\": 18, \"duration_ms\": 800, \"period_ms\": 200}",
     );
 
     // set up tcp listener
@@ -89,14 +52,15 @@ fn main() {
         let code_change_pressed = button::poll(26);
         println!("CODE CHANGE BUTTON: {}", code_change_pressed);
         if code_change_pressed {
-            let data_tmp = communication::connect(&gui_address, data.to_string());
+            let data_tmp = communication::connect_with_gui(&gui_address, data.to_string());
             data = String::from(data_tmp);
         }
         let light_activate_pressed = button::poll(27);
         println!("LIGHT ACTIVATE BUTTON: {}", light_activate_pressed);
         if light_activate_pressed {
             let light_args: light::LightArguments = serde_json::from_str(&data).unwrap();
-            light::blink_led(light_args);
+            communication::connect_with_light(&light_args.host, data.to_string());
+            // light::blink_led(light_args);
         }
         println!("");
     });
@@ -165,62 +129,3 @@ fn main() {
     // forever (until ctrl-c is pressed).
     tokio::run(done);
 }
-
-// ========================================================================================
-
-// Thread Stuff
-
-// use std::sync::Arc;
-// use std::sync::Mutex;
-// use std::thread;
-// use std::time::Duration;
-
-// fn main() {
-//     let mut foo: Vec<i32> = Vec::new();
-//     foo.push(34);
-//     foo.push(56);
-
-//     let data = Arc::new(Mutex::new(foo));
-
-//     for i in 0..5 {
-//         let dd = data.clone();
-//         let ddd = data.clone();
-//         let index = i;
-
-//         thread::spawn(move || {
-//             println!("spawned consumer thread {}", index);
-
-//             loop {
-//                 let mut d = dd.lock().unwrap();
-
-//                 if d.len() == 0 {
-//                     println!("no work for thread {}, sleeping", i);
-//                     thread::sleep(Duration::from_secs(1));
-//                 } else {
-//                     let x: i32 = d.pop().unwrap();
-
-//                     println!("thread {} has work!  {}", index, x);
-//                     thread::sleep(Duration::from_secs(x as u64));
-//                     println!("thread {} work complete!", index);
-//                 }
-//             }
-//         });
-
-//         thread::spawn(move || {
-//             println!("spawned producer thread {}", index);
-
-//             loop {
-//                 let mut d = ddd.lock().unwrap();
-
-//                 if d.len() == 0 {
-//                     println!("thread {} has work!", index);
-//                     d.push(34);
-//                     println!("thread {} work complete!", index);
-//                 } else {
-//                     println!("no work for thread {}, sleeping", i);
-//                     thread::sleep(Duration::from_secs(1));
-//                 }
-//             }
-//         });
-//     }
-// }
